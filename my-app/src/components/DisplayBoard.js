5e8cb97f663b0fe268f57bf5dd4d10a3d49075eb
shinnh2 <nahyeon.shin9@gmail.com>
2022-12-09 00:42:06 +0900

    컴포넌트 생성

--- my-app/src/components/DisplayBoard.js
+++ my-app/src/components/DisplayBoard.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const DisplayBoard = ({numberOfBooks, getAllBook}) => {
-    
-    return(
-        <div className="display-wrapper">
-            <div className="display-box">
-                <div className="display-board">
-                    <h4>생성된 수</h4>
-                    <div className="number">
-                    {numberOfBooks}
-                    </div>
-                </div>
-                <div className="get-button">
-                    <button onClick={() => getAllBook()}>Get all Books</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+const DisplayBoard = ({
+	numberOfBooks,
+	numberOfTodos,
+	getAllBook,
+	getAllTodo,
+}) => {
+	return (
+		<div className="display-wrapper">
+			<div className="display-box">
+				<div className="display-board">
+					<h4 className="display_title">생성된 book수</h4>
+					<div className="number">{numberOfBooks}권</div>
+					<h4 className="display_title">생성된 todo수</h4>
+					<div className="number">{numberOfTodos}개</div>
+				</div>
+				<div className="get-button">
+					<button onClick={() => getAllBook()}>Get all Books</button>
+				</div>
+				<div className="get-button">
+					<button onClick={() => getAllTodo()}>Get all Todos</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default DisplayBoard;
